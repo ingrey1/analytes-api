57667b59f4e17ee996e4ed3ff596af39000dfebc
Jason Mortensen <ingrey1@gmail.com>
2022-08-05 12:20:06 -0700

    add cors middlware

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
-
+var cors = require('cors')
 const analytes = require('./routes/analytes');
 
 
 const app = express();
+
+app.use(cors())
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
